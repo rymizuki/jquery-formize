39eba3a5db2cb1c52dc1caf3bd864202e34776c1
mizuki_r <ry.mizuki@gmail.com>
2013-11-19 21:03:21 +0900

    added methods

--- src/formize.js
+++ src/formize.js
@@ -7,32 +7,55 @@
  */
 
 (function($) {
+  'use strict';
+
+  var Formize = (function () {
+      var Constructor = function ($el, options) {
+          this.$el = $el;
+      };
+
+      Constructor.prototype.get_value = function (name) {
+          return this.$el.find('[name="'+name+'"]').val();
+      };
+
+      Constructor.prototype.get_values = function () {
+          var data = {};
+          this.$el.find('[name]').each(function () {
+              var $this = $(this);
+              data[$this.attr('name')] = $this.val();
+          });
+          return data;
+      };
+
+      Constructor.prototype.set_value = function (name, value) {
+          this.$el.find('[name="'+name+'"]').val(value);
+      };
+
+      Constructor.prototype.set_values = function (data) {
+          $.each(data, $.proxy(this.set_value, this));
+      };
+
+      return Constructor;
+  })(); 
+
 
   // Collection method.
-  $.fn.formize = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  $.fn.formize = function(action, value) {
+    return this.each(function() {
+        var $this = $(this),
+            data  = $this.data('formize.data-api');
+
+        if (!data) {
+            data = new Formize($this);
+            $this.data('formize.data-api', data);
+        }
+
+        data[action](value);
     });
   };
 
   // Static method.
-  $.formize = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.formize.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
+  $.formize = function(selector) {
+      return new Formize($(selector));
   };
-
-  // Static method default options.
-  $.formize.options = {
-    punctuation: '.'
-  };
-
-  // Custom selector.
-  $.expr[':'].formize = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
-
 }(jQuery));
